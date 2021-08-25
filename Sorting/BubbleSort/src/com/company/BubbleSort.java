ce49fdcfc49de4d00d4ea8f20cb1e5555d409bad
Nipesh Maharjan <88616145+nipu-pasa@users.noreply.github.com>
2021-08-25 13:53:02 +0545

    Modify the Files

--- Sorting/BubbleSort/src/com/company/BubbleSort.java
+++ Sorting/BubbleSort/src/com/company/BubbleSort.java
@@ -2,38 +2,13 @@ package com.company;
 
 import java.util.Arrays;
 
-public class Main {
+public class BubbleSort {
 
     public static void main(String[] args) {
-        int[] arr = {-32,100,-2,3,44};
+        int[] arr = {3, 1, 5, 4, 2};
         bubble(arr);
         System.out.println(Arrays.toString(arr));
     }
-
-    static void selection(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            // find the max item in the remaining array and swap with the correct index
-            int last = arr.length - i - 1;
-            int maxIndex = getMaxIndex(arr, 0, last);
-            swap(arr,maxIndex,last);
-        }
-    }
-    static void swap(int[] arr,int first,int second){
-        int temp =arr[first];
-        arr[first] =arr[second];
-        arr[second]=temp;
-    }
-
-    static int getMaxIndex(int[] arr, int start, int end) {
-        int max = start;
-        for (int i = start; i <= end; i++) {
-            if (arr[max] < arr[i]){
-                max = i;
-        }
-    }
-    return max;
-}
-
 
     static void bubble(int[] arr) {
         boolean swapped;
@@ -56,6 +31,5 @@ public class Main {
                 break;
             }
         }
-
     }
 }
